041b082a93a6fa44edc4982a301703dfe6233d6e
Jonathan Ikpen <jonathanikpen4@gmail.com>
2024-10-14 16:44:50 +0100

    fix: change helpline

--- constant.ts
+++ constant.ts
@@ -1,3 +1,3 @@
 // export const HELP_LINK="https://wa.me/2348127964509"
-export const HELP_LINK="https://wa.me/2349075426824"
+export const HELP_LINK="https://wa.me/2348060848383"
 export const API_URL=""
